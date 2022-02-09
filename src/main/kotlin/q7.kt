864c187e66956adc89c09f753546b2ea235da8f2
razie rezaee <rezaie127@yahoo.com>
2022-02-09 22:12:51 +0330

    write main function with try catch block

--- src/main/kotlin/q7.kt
+++ src/main/kotlin/q7.kt
@@ -1,2 +1,31 @@
-class q7  {
+import java.util.*
+
+fun main() {
+    try {
+        var flag = true
+        while (flag) {
+            val r = Scanner(System.`in`)
+            println("\nenter 1 for convert Decimal to Binary ")
+            println("enter 2 for convert Binary to Decimal ")
+            println("enter 0 for exit ")
+            when (r.nextInt()) {
+                1 -> {
+                    decimalToBinary()
+                }
+                2 -> {
+                    binaryToDecimal()
+                }
+                0 -> {
+                    flag = false
+                }
+                else -> {
+                    println("number is incorrect")
+                }
+            }
+        }
+    } catch (e: InputMismatchException) {
+        println("input invalid")
+    } catch (e: Exception) {
+        println("input invalid")
+    }
 }
