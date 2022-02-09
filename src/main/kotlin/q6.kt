5d3efb7cee19e0ba0824f17739b6979ae0fd11b4
razie rezaee <rezaie127@yahoo.com>
2022-02-09 22:10:21 +0330

    write the answer

--- src/main/kotlin/q6.kt
+++ src/main/kotlin/q6.kt
@@ -1,2 +1,29 @@
-class q6  {
+fun main() {
+
+    //O(1)
+    val n: Int = 5
+    println("Your input is: $n")
+
+
+    //O(n)
+    for (i in 0 until n) {
+        println("Your output is $i")
+    }
+
+
+    //O(n!)
+    for (i in 1..n) {
+        for (j in 1..n) {
+            println("Your output is $i and $j")
+        }
+    }
+    for (i in 1..factorial(n)) {
+        println("Your output is $i")
+    }
+
+}
+
+
+fun factorial(n:Int):Long{
+    return 12
 }
