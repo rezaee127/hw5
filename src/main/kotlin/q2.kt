71cf72a3f88fa242a500937b805309f6e94a08ea
razie rezaee <rezaie127@yahoo.com>
2022-02-09 20:53:14 +0330

    create employees & list of Employees

--- src/main/kotlin/q2.kt
+++ src/main/kotlin/q2.kt
@@ -22,11 +22,15 @@ class Clerk(override val name: String, override val age: Int, override val hourR
 
 data class Customer(var name:String,var age:Int,var gender:Gender)
 
+fun main() {
+    val manager1 = Manager("Ali", 52, 104.0)
+    val clerk1 = Clerk("Sama", 26, 110.0)
+    val manager2 = Manager("Reza", 42, 120.0)
+    val manager3 = Manager("Farhad", 34, 120.0)
+    val listOfEmployee = arrayOf(manager1, clerk1, manager2, manager3)
 
-
-
-
-
+    val listOfHours = arrayOf(140.0, 150.0, 130.0, 180.0)
+}
 
 
 
