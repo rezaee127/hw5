03aa6d6cfbebba3f52d6b309ba6ad5523429d365
razie rezaee <rezaie127@yahoo.com>
2022-02-09 22:02:11 +0330

    write generic extension function

--- src/main/kotlin/q5.kt
+++ src/main/kotlin/q5.kt
@@ -1,2 +1,12 @@
-class q5  {
+fun <F> ArrayList<F>.slice2(i:Int){
+    if (i>-1) {
+        if (i+1<this.size) {
+            val x = this.subList(0, i + 1)
+            val y = this.subList(i + 1, this.lastIndex + 1)
+            print("[$x , $y]\n")
+        }else{
+            print("${this}\n")
+        }
+    }else
+        print("slice is impossible")
 }
