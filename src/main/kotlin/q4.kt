39789ea92f25d05c9a7ae3b97bae198bb445a37f
razie rezaee <rezaie127@yahoo.com>
2022-02-09 21:59:43 +0330

    write generic extension function

--- src/main/kotlin/q4.kt
+++ src/main/kotlin/q4.kt
@@ -1,2 +1,10 @@
-class q4   {
+
+fun <T> ArrayList<T>.print(){
+    for (t in this)
+        print("$t    ")
+}
+
+fun main(){
+    val list= arrayListOf(32, "Ali" , 334.89 , 'Z' , -456)
+    list.print()
 }
