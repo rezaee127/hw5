39ef4f7fb6c1acb2141d5b14eff226573856304e
razie rezaee <rezaie127@yahoo.com>
2022-02-09 18:52:42 +0330

    create interfaces

--- src/main/kotlin/q1.kt
+++ src/main/kotlin/q1.kt
@@ -1,2 +1,23 @@
-class q1 {
+interface Vehicle{
+    fun maxSpeed()
+    fun maxCapacity()
+    val name:String
+    val speed:Int
+    val capacity:Int
+}
+
+interface PublicTrasportationVehicle:Vehicle {
+    override fun maxSpeed()
+    override fun maxCapacity()
+    override val name:String
+}
+interface ServiceTrasportationVehicle:Vehicle {
+    override fun maxSpeed()
+    override fun maxCapacity()
+    override val name:String
+}
+interface PrivateTrasportationVehicle:Vehicle {
+    override fun maxSpeed()
+    override fun maxCapacity()
+    override val name:String
 }
