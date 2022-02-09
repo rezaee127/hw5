45c971760405d774cea9068138ad9295ae2dc7a1
razie rezaee <rezaie127@yahoo.com>
2022-02-09 22:07:21 +0330

    write list for test

--- src/main/kotlin/q5.kt
+++ src/main/kotlin/q5.kt
@@ -10,3 +10,16 @@ fun <F> ArrayList<F>.slice2(i:Int){
     }else
         print("slice is impossible")
 }
+
+
+fun main(){
+    val list= arrayListOf(32, "Ali" , 334.89 , 'Z' , -456)
+    list.slice2(1)
+    val list2= arrayListOf(1,2,3,4,5)
+    list2.slice2(2)
+    val list3= arrayListOf('a','b','c','d','e','f','g')
+    list3.slice2(3)
+    list3.slice2(20)
+    list3.slice2(0)
+    list3.slice2(-1)
+}
